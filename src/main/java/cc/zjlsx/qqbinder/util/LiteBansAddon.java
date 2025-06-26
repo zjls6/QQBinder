337a334fdeea6b85ea9b5812b893dc3cabbfcc44
zjls6 <zjls6@qq.com>
2025-06-26 13:57:18 +0800

    add README.md

--- src/main/java/cc/zjlsx/qqbinder/util/LiteBansAddon.java
+++ src/main/java/cc/zjlsx/qqbinder/util/LiteBansAddon.java
@@ -1,7 +1,6 @@
 package cc.zjlsx.qqbinder.util;
 
 import litebans.api.Database;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,26 +8,20 @@ import java.util.UUID;
 
 public class LiteBansAddon {
     public static boolean isBanedByConsole(UUID uuid) {
-        String query = "SELECT * FROM {bans} where uuid=? and banned_by_uuid=? and removed_by_uuid is null order by time desc";
-        try (PreparedStatement st = Database.get().prepareStatement(query)) {
-            st.setString(1, uuid.toString());
-            st.setString(2, "CONSOLE");
-            try (ResultSet rs = st.executeQuery()) {
-                return rs.next();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
+        String query = "SELECT COUNT(*) FROM {bans} WHERE uuid=? AND banned_by_uuid='CONSOLE' AND removed_by_uuid IS NULL";
+        return checkIfBanned(uuid, query);
     }
 
     public static boolean isPermanentlyBaned(UUID uuid) {
-        String query = "SELECT * FROM {bans} where uuid=? and until=? and removed_by_uuid is null order by time desc";
+        String query = "SELECT COUNT(*) FROM {bans} WHERE uuid=? AND until=0 AND removed_by_uuid IS NULL";
+        return checkIfBanned(uuid, query);
+    }
+
+    private static boolean checkIfBanned(UUID uuid, String query) {
         try (PreparedStatement st = Database.get().prepareStatement(query)) {
             st.setString(1, uuid.toString());
-            st.setInt(2, 0);
             try (ResultSet rs = st.executeQuery()) {
-                return rs.next();
+                return rs.next() && rs.getInt(1) > 0;
             }
         } catch (SQLException e) {
             e.printStackTrace();
